4da3752128dd4aaa4063218470a3413c05d2d034
varshap-17 <varshap630@gmail.com>
2022-11-14 18:29:45 +0530

    cart exception class

---
+++ src/main/java/com/example/cartmodule/exception/CartDataException.java
@@ -0,0 +1,7 @@
+package com.example.cartmodule.exception;
+
+public class CartDataException extends RuntimeException{
+    public CartDataException(String message) {
+        super(message);
+    }
+}
